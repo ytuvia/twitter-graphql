4189908dcfcf41b8dbc3bbe764bcfa64eef34a90
Yehuda Tuvia <ytuvia@gmail.com>
2017-03-17 21:34:50 +0200

    back to def query

--- js/routes/AppHomeRoute.js
+++ js/routes/AppHomeRoute.js
@@ -4,19 +4,8 @@ export default class extends Relay.Route {
   static queries = {
     tweet: () => Relay.QL`
         query TwitterAPI{
-		 search(q:"trump", count: 1000){
-		  	tweet{
-		      id,
-		      user {
-		        id,
-		        followers_count,
-		        tweets(limit:5) {
-		          retweet_count
-		        }
-		      }
-		    }
-			}
-		}
+            search(q: "graphQl")
+        }
     `,
   };
   static routeName = 'AppHomeRoute';
